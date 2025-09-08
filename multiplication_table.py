c7c1c2518acc78bda47bb08798c2739ea90b2417
samsamhahaha <ambdhhj7@gmail.com>
2025-09-09 00:23:33 +0800

    multiplication_table.py

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,3 +1,8 @@
-n = int(input("enter the number: "))
-for i in range(1, 11):
-    print(n, "*", i, "=", n*i)
+while True:
+    n = int(input("enter the number: "))
+    for i in range(1, 11):
+        print(n, "*", i, "=", n*i)
+
+    again = input("again? (y/n): ")
+    if again.lower() == "n":
+        break
